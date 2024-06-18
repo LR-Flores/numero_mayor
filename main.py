c161c7cc47ceaf4d9ebc8de275ee2c2cac31fd4a
Luis Roberto Flores <lr_flores@outlook.com>
2024-06-18 11:59:13 -0600

    And so, it begins.

---
+++ main.py
@@ -0,0 +1,12 @@
+# numero1 = int(input("proporciona el primer valor: "))
+# numero2 = int(input("proporciona  el segundo valor: "))
+
+
+numero1 = int(input("proporciona el primer valor: "))
+numero2 = int(input("proporciona el segundo numero: "))
+
+if numero1 > numero2:
+    print("el primer valor es mayor ")
+else:
+    print("el segundo valor es mayor ")
+    print("hola")
